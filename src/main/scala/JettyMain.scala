7acddeafc21a04a24072a3da1ede79c99f6643b7
Brian Schlining <bschlining@gmail.com>
2021-10-05 16:27:37 -0700

    Removed unused param

--- src/main/scala/JettyMain.scala
+++ src/main/scala/JettyMain.scala
@@ -49,7 +49,7 @@ object JettyMain {
     connector setIdleTimeout conf.connectorIdleTimeout
     server addConnector connector
 
-    val webapp = conf.webapp
+    // val webapp = conf.webapp
     val webApp = new WebAppContext
     webApp setContextPath conf.contextPath
     webApp setResourceBase conf.webapp

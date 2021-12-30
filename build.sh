d29b12c89df9afc8aecd0e2c5f9496ced57386f4
Brian Schlining <bschlining@gmail.com>
2021-12-30 15:08:41 -0800

    Using docker buildx

--- build.sh
+++ build.sh
@@ -5,10 +5,17 @@ echo "--- Building panoptes (reminder: run docker login first!!)"
 BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 VCS_REF=`git tag | sort -V | tail -1`
 
+# sbt pack && \
+#     docker build --build-arg BUILD_DATE=$BUILD_DATE \
+#                  --build-arg VCS_REF=$VCS_REF \
+#                   -t mbari/panoptes:${VCS_REF} \
+#                   -t mbari/panoptes:latest . && \
+#     docker push mbari/panoptes
+
 sbt pack && \
-    docker build --build-arg BUILD_DATE=$BUILD_DATE \
-                 --build-arg VCS_REF=$VCS_REF \
-                  -t mbari/panoptes:${VCS_REF} \
-                  -t mbari/panoptes:latest . && \
-    docker push mbari/panoptes
+    docker buildx build \
+        --platform linux/amd64,linux/arm64 \
+        --push \
+        -t mbari/panoptes:${VCS_REF} \
+        -t mbari/panoptes:latest . 
 
